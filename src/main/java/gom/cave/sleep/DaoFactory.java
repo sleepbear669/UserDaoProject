b9f6eda1d3fd9df0ba42d4d7994d5decd937be44
sleepbear <sleepbear669@gmail.com>
2016-04-08 17:26:03 +0900

    Use spring context

--- src/main/java/gom/cave/sleep/DaoFactory.java
+++ src/main/java/gom/cave/sleep/DaoFactory.java
@@ -1,10 +1,20 @@
 package gom.cave.sleep;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by sleepbear on 2016. 4. 1..
  */
+@Configuration
 public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(new SimpleConnectionMaker());
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
+    }
+
+    @Bean
+    public SimpleConnectionMaker connectionMaker() {
+        return new SimpleConnectionMaker();
     }
 }
--- src/test/java/gom/cave/sleep/UserDaoTest.java
+++ src/test/java/gom/cave/sleep/UserDaoTest.java
@@ -1,6 +1,9 @@
 package gom.cave.sleep;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -9,22 +12,36 @@ import static org.hamcrest.core.Is.is;
  * Created by sleepbear on 2016. 3. 25..
  */
 public class UserDaoTest {
+
+    private UserDao userDao;
+
+    @Before
+    public void setUp() throws Exception {
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("DaoFactory.xml");
+        userDao = (UserDao) context.getBean("userDao");
+//        userDao = new DaoFactory().getUserDao();
+    }
+
     @Test
     public void testUserGet() throws Exception {
         // Given
-        final UserDao userDao = new DaoFactory().getUserDao();
         final long id = 1L;
         final String name = "kim";
         final String password= "gom";
 
         // When
-        User user =userDao.get(id);
+        User user = userDao.get(id);
 
         // Then
+        resultUserMatchCheck(id, name, password, user);
+
+    }
+
+    private void resultUserMatchCheck(long id, String name, String password, User user) {
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
-
     }
 
     @Test
@@ -35,15 +52,12 @@ public class UserDaoTest {
         final User user = new User();
         user.setName(name);
         user.setPassword(password);
-        final UserDao userDao = new DaoFactory().getUserDao();
 
         // When
         Long resultId = userDao.add(user);
         final User resultUser = userDao.get(resultId);
         // Then
-        assertThat(resultUser.getId(), is(resultId));
-        assertThat(resultUser.getName(), is(name));
-        assertThat(resultUser.getPassword(), is(password));
+        resultUserMatchCheck(resultId, name, password, resultUser);
     }
 
 
